3da9e638ebf02a7ad1f0dda894f3b2e8e075a43e
Harrison Lingren <b00745342@scm.net>
2017-11-27 17:40:50 +0000

    sample to-do functionality

--- app.js
+++ app.js
@@ -5,25 +5,42 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+var faker = require('faker');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+function makeItems(n) {
+  items = [];
+  for (var i = 0; i < n; i++) {
+    let newItem = {
+      id: i,
+      title: faker.lorem.words(),
+      done: faker.random.boolean()
+    };
+
+    items.push(newItem);
+  }
+
+  return items;
+}
+
+var items = makeItems(8);
+
+var index = require('./routes/index')(items);
+var item = require('./routes/item')(items);
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/item', item);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = (items) => {
+  
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', {
+      title: 'COM 642 Assignment 2 | A16',
+      todoItems: items
+    });
+  });
 
-module.exports = router;
+  return router;
+};
