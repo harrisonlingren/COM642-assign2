f09c7e936ccbf0b8a7457bf8d339c3321de3fb15
Patterson Gareth <B00616398@scm.net>
2017-11-28 18:55:29 +0000

    Added strikethrough for text as well as header

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
+//Mark Item as done 
 $(document).ready (function(){
 $("input[type= 'checkbox']").click(function (){
   var titlenum = this.id[this.id.length -1]
-  $('#itemtitle'+titlenum).toggleClass('titledone')
+  $('#itemtitle'+titlenum).toggleClass('done')
+  var itemtext = this.id[this.id.length -1]
+  $('#itemtext'+titlenum).toggleClass('done')
+  $("#itemtitle").parent().css({"color": "red", "border": "2px solid red"});
 })
-});
+})
+
