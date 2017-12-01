74ecf8683c60002b05851157b6cc2774c92776b0
Harrison Lingren <b00745342@scm.net>
2017-12-01 16:17:11 +0000

    stable, bugs fixed

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,17 +1,22 @@
 $(document).ready( () => {
     // load cards
-    loadTodoCards();
+    updateCards();
 });
 
-const todoItemsData = [];
+// array to hold cards as cached data
+var todoItemsData = [];
 
-function loadTodoCards() {
+// Refresh cards container by getting all cards via AJAX
+function updateCards() {
     // GET endpoint for all to-do items
     $.getJSON('/item/all', (res) => {
+        todoItemsData = [];
+        $('.cardContainer').empty();
         // iterate over array of to-do items and insert '.card' elements
         $.each(res.data, (idx, item) => {
+            todoItemsData.push(item);
             createNewCard(item);
-        });
+        }); alerts(); initCardEvents();
     });
 }
 
@@ -36,7 +41,7 @@ function initCardEvents() {
 
         let cardSelector = '.card[data-todoid="' +todoItemId + '"]';
         let itemDone = $(cardSelector + ' input[type="checkbox"]').is(':checked');
-        console.log($(cardSelector + ' input[type="checkbox"]'), itemDone);
+        
         $.ajax({
             url: '/item/'+todoItemId,
             type: 'PUT',
@@ -88,17 +93,21 @@ function initCardEvents() {
         let thisCard = $(e.currentTarget).parent();
         let todoItemId = parseInt(thisCard.data('todoid'));
 
-        console.log('editing: ', thisCard, todoItemId);
-
         $('#edit-modal button.btn-primary').attr('data-editmode', 'edit');
+        $('#edit-modal button.btn-primary').attr('data-todoid', todoItemId);
+
+        let todoItem = todoItemsData[todoItemId];
+        let d = new Date(todoItem.date).toISOString().substring(0,10);
+        $('#item-title').val(todoItem.title);
+        $('#item-category').val(todoItem.category);
+        $('#item-date').val( d );
+        $('#item-description').val(todoItem.description);
         $('#edit-modal button.btn-primary').attr('data-todoid', todoItemId);
     });
 }
 
 function createNewCard(item) {
     // push item to cache
-    todoItemsData.push(item);
-
     let container = $('.cardContainer');
     
     let cardTitle = $('<h4 class="card-title"></h4>').text(item.title);
@@ -117,7 +126,7 @@ function createNewCard(item) {
         .attr('data-todoid', item.item_id)
         .append('<i class="fa fa-pencil"></i>');
 
-    let card = $('<div class="card"></div>')
+    let card = $('<div class="card w-25 p-3"></div>')
         .attr('data-todoid', item.item_id)
         .append(cardTitle)
         .append(cardClose)
@@ -127,12 +136,8 @@ function createNewCard(item) {
         .append(cardBox);
 
     container.append(card);
-    initCardEvents(); updateCards();
 }
 
 function parseBoolean(b) {
     return (b == 'true' || b == true);
 }
-
-
-
